3da5a50fa6f383005769466ed4e475cc7d4ac81f
Alexander Searle <41946602+nightcry15@users.noreply.github.com>
2023-04-04 14:34:11 +0200

    In-progress

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*Typed js*/
+const typed = new Typed('.multiple-text', {
+    strings: ['Smoked Cheeses', 'Smoked Spices', 'Other'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
